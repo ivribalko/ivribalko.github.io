3e104c78b6e905de4faf6409faa00dc3f1246307
Ivan Rybalko <ivribalko@gmail.com>
2020-08-02 18:18:06 -0400

    set locale

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -8,8 +8,11 @@ import 'l10n.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      Get.updateLocale(Localizations.localeOf(Get.context));
+    });
     return GetMaterialApp(
-      title: 'Ivan Rybalko',
+      title: 'Ivan Rybalko - Flutter developer in NY',
       translationsKeys: translationsKeys,
       theme: theme,
       home: Home(),
