9028e20c247ce24543a6afdf61d6a50e034afd82
Ivan Rybalko <ivribalko@gmail.com>
2020-08-06 16:50:40 -0400

    IgnorePointer

--- lib/ui/home/next_page.dart
+++ lib/ui/home/next_page.dart
@@ -12,27 +12,31 @@ class NextPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Obx(
       () {
-        return AnimatedOpacity(
-          duration: kDuration,
-          opacity: scrolling.isFooter.value ? 0 : 1,
-          child: Align(
-            alignment: Alignment.bottomCenter,
-            child: padded(
-              child: SizedBox(
-                width: kFooterHeight,
-                height: kFooterHeight,
-                child: AnimatedContainer(
-                  decoration: _buildBoxDecoration(),
-                  duration: kDuration,
-                  curve: Curves.easeIn,
-                  child: Material(
-                    color: Colors.transparent,
-                    child: IconButton(
-                      icon: Icon(Icons.arrow_downward),
-                      onPressed: () => scroll.animateToPage(
-                        scroll.page.toInt() + 1,
-                        duration: kDuration,
-                        curve: Curves.easeOutQuad,
+        var isFooter = scrolling.isFooter.value;
+        return IgnorePointer(
+          ignoring: isFooter ? true : false,
+          child: AnimatedOpacity(
+            duration: kDuration,
+            opacity: isFooter ? 0 : 1,
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: padded(
+                child: SizedBox(
+                  width: kFooterHeight,
+                  height: kFooterHeight,
+                  child: AnimatedContainer(
+                    decoration: _buildBoxDecoration(),
+                    duration: kDuration,
+                    curve: Curves.easeIn,
+                    child: Material(
+                      color: Colors.transparent,
+                      child: IconButton(
+                        icon: Icon(Icons.arrow_downward),
+                        onPressed: () => scroll.animateToPage(
+                          scroll.page.toInt() + 1,
+                          duration: kDuration,
+                          curve: Curves.easeOutQuad,
+                        ),
                       ),
                     ),
                   ),
@@ -46,22 +50,19 @@ class NextPage extends StatelessWidget {
   }
 
   BoxDecoration _buildBoxDecoration() {
+    var isHeader = scrolling.isHeader.value;
     return BoxDecoration(
       color: Get.theme.scaffoldBackgroundColor,
       borderRadius: BorderRadius.all(Radius.circular(
-        scrolling.isHeader.value ? 0 : 45,
+        isHeader ? 0 : 45,
       )),
       boxShadow: [
         BoxShadow(
-          color: scrolling.isHeader.value
-              ? Colors.transparent
-              : Get.theme.accentColor,
+          color: isHeader ? Colors.transparent : Get.theme.accentColor,
           offset: Offset(0, 3),
         ),
         BoxShadow(
-          color: scrolling.isHeader.value
-              ? Colors.transparent
-              : Get.theme.accentColor,
+          color: isHeader ? Colors.transparent : Get.theme.accentColor,
           offset: Offset(0, -2),
         ),
       ],
