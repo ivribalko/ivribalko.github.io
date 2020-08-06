b1b8e73fe3ce0489e4a3c193a33571b4ab8fba8e
Ivan Rybalko <ivribalko@gmail.com>
2020-08-06 16:20:09 -0400

    animate border

--- lib/ui/home/next_page.dart
+++ lib/ui/home/next_page.dart
@@ -17,23 +17,51 @@ class NextPage extends StatelessWidget {
         }
         return Align(
           alignment: Alignment.bottomCenter,
-          child: SizedBox(
-            width: kFooterHeight,
-            height: kFooterHeight,
-            child: Material(
-              color: Colors.transparent,
-              child: IconButton(
-                icon: Icon(Icons.arrow_downward),
-                onPressed: () => scroll.animateToPage(
-                  scroll.page.toInt() + 1,
-                  duration: kDuration,
-                  curve: Curves.easeOutQuad,
+          child: padded(
+            child: SizedBox(
+              width: kFooterHeight,
+              height: kFooterHeight,
+              child: AnimatedContainer(
+                decoration: _buildBoxDecoration(),
+                duration: kDuration,
+                curve: Curves.easeIn,
+                child: Material(
+                  color: Colors.transparent,
+                  child: IconButton(
+                    icon: Icon(Icons.arrow_downward),
+                    onPressed: () => scroll.animateToPage(
+                      scroll.page.toInt() + 1,
+                      duration: kDuration,
+                      curve: Curves.easeOutQuad,
+                    ),
+                  ),
                 ),
               ),
             ),
           ),
         );
       },
+    );
+  }
+
+  BoxDecoration _buildBoxDecoration() {
+    return BoxDecoration(
+      color: Get.theme.scaffoldBackgroundColor,
+      borderRadius: BorderRadius.all(Radius.circular(
+        scrolling.isHeader.value ? 0 : 45,
+      )),
+      boxShadow: [
+        if (!scrolling.isHeader.value)
+          BoxShadow(
+            color: Get.theme.accentColor,
+            offset: Offset(0, 3),
+          ),
+        if (!scrolling.isHeader.value)
+          BoxShadow(
+            color: Get.theme.accentColor,
+            offset: Offset(0, -2),
+          ),
+      ],
     );
   }
 }
