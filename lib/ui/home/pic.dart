220f8907d456336aa01c083b8251bb9b9893a1ae
Ivan Rybalko <ivribalko@gmail.com>
2020-08-08 10:41:42 -0400

    load fade image

--- lib/ui/home/pic.dart
+++ lib/ui/home/pic.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class Pic extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Image.asset('assets/vga.png'));
+    return Center(
+      child: Stack(
+        children: <Widget>[
+          Center(child: CircularProgressIndicator()),
+          Center(
+            child: FadeInImage.memoryNetwork(
+              placeholder: kTransparentImage,
+              image: 'assets/vga.png',
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
