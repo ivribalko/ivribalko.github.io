c3d91aca91455a29e9f700398fe99673d4632522
Ivan Rybalko <ivribalko@gmail.com>
2020-08-08 11:03:18 -0400

    ignore fab

--- lib/ui/home/fab.dart
+++ lib/ui/home/fab.dart
@@ -12,30 +12,33 @@ class FAB extends StatelessWidget {
     return Obx(() {
       var isHeader = scrolling.isHeader.value;
       var isFooter = scrolling.isFooter.value;
-      return AnimatedOpacity(
-        duration: kDuration,
-        curve: kCurve,
-        opacity: isFooter ? 0 : 1,
-        child: Padding(
-          padding: EdgeInsets.only(bottom: kPadding / 2),
-          child: FloatingActionButton.extended(
-            onPressed: mail,
-            label: AnimatedCrossFade(
-              duration: kDuration,
-              firstCurve: kCurve,
-              secondCurve: kCurve,
-              sizeCurve: kCurve,
-              firstChild: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Icon(Icons.email),
-                  if (isHeader) Text('email_me'.tr),
-                ]..addSpacing(factor: 0.5),
+      return IgnorePointer(
+        ignoring: isFooter,
+        child: AnimatedOpacity(
+          duration: kDuration,
+          curve: kCurve,
+          opacity: isFooter ? 0 : 1,
+          child: Padding(
+            padding: EdgeInsets.only(bottom: kPadding / 2),
+            child: FloatingActionButton.extended(
+              onPressed: mail,
+              label: AnimatedCrossFade(
+                duration: kDuration,
+                firstCurve: kCurve,
+                secondCurve: kCurve,
+                sizeCurve: kCurve,
+                firstChild: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(Icons.email),
+                    if (isHeader) Text('email_me'.tr),
+                  ]..addSpacing(factor: 0.5),
+                ),
+                secondChild: Icon(Icons.email),
+                crossFadeState: isHeader
+                    ? CrossFadeState.showFirst
+                    : CrossFadeState.showSecond,
               ),
-              secondChild: Icon(Icons.email),
-              crossFadeState: isHeader
-                  ? CrossFadeState.showFirst
-                  : CrossFadeState.showSecond,
             ),
           ),
         ),
