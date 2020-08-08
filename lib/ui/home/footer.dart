1180a6dcf18b53bc6214d630e0485a3ff3f59629
Ivan Rybalko <ivribalko@gmail.com>
2020-08-08 10:43:50 -0400

    bold

--- lib/ui/home/footer.dart
+++ lib/ui/home/footer.dart
@@ -43,7 +43,10 @@ class Footer extends StatelessWidget {
     ),
     MaterialButton(
       onPressed: mail,
-      child: Text('ivan@rybalko.dev'),
+      child: Text(
+        'ivan@rybalko.dev',
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
     ),
   ];
 
