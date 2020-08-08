298f8e7db61badb874c03117ab55d3850ff1280e
Ivan Rybalko <ivribalko@gmail.com>
2020-08-08 11:01:24 -0400

    applyButtonShape

--- lib/ui/theme.dart
+++ lib/ui/theme.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
 
 final Map<ThemeMode, ThemeData> themes = {
-  ThemeMode.light: ThemeData.light(),
-  ThemeMode.dark: ThemeData.dark(),
+  ThemeMode.light: ThemeData.light().applyButtonShape(),
+  ThemeMode.dark: ThemeData.dark().applyButtonShape(),
 };
+
+extension Customize on ThemeData {
+  ThemeData applyButtonShape() {
+    return copyWith(
+      buttonTheme: buttonTheme.copyWith(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(25),
+          ),
+        ),
+      ),
+    );
+  }
+}
