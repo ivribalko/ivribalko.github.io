4ea879ee17618ecc61bf79165a31325059fde518
Ivan Rybalko <ivribalko@gmail.com>
2020-08-09 11:21:46 -0400

    SharedAxisTransition

--- lib/ui/home/animated_text.dart
+++ lib/ui/home/animated_text.dart
@@ -1,3 +1,4 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -16,13 +17,30 @@ class AnimatedText extends StatefulWidget {
   _AnimatedTextState createState() => _AnimatedTextState();
 }
 
-class _AnimatedTextState extends State<AnimatedText> {
+class _AnimatedTextState extends State<AnimatedText>
+    with SingleTickerProviderStateMixin {
+  AnimationController animation;
+
+  _AnimatedTextState() {
+    animation = AnimationController(
+      duration: kDuration,
+      vsync: this,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return AnimatedSwitcher(
-      duration: kDuration,
-      switchInCurve: kCurve,
-      switchOutCurve: kCurve,
+      duration: kDuration * 1.5,
+      transitionBuilder: (child, animation) {
+        return SharedAxisTransition(
+          animation: animation,
+          secondaryAnimation: this.animation,
+          transitionType: SharedAxisTransitionType.vertical,
+          fillColor: Colors.transparent,
+          child: child,
+        );
+      },
       child: Text(
         widget.items[widget.index],
         key: ValueKey(widget.index),
